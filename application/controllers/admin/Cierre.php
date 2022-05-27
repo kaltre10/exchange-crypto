e4cae421e8c08b524d988eb40c9be014e25a220a
Jason Hernandez <kaltre10@gmail.com>
2022-05-27 02:13:57 -0500

    cierre del dia

--- application/controllers/admin/Cierre.php
+++ application/controllers/admin/Cierre.php
@@ -42,61 +42,140 @@ class Cierre extends CI_Controller {
 					$i++;	
 				}
 			}
-	
-			$caja = sumar_divisa($divisas, $operaciones, $ent_sal, $cuentas, $cierre);
 
-			$i = 0;
+			$array = sumar_divisa($divisas, $operaciones, $ent_sal, $cuentas, $cierre);
 
-			foreach ($caja as $key) {
-		
-				$fecha = date("Y-m-d");
-				
-				if ($key["caja"] != 0) {
+			//calculo de los 5 ultimos cierres
+			$index = 0;
+			$cierres = [];
+			if(count($hay_datos) > 0){
+				$row = array_column($hay_datos, 'fec_cierre'); //primer registro 
+				$row[0]; //primera fecha del cierre insertado para detener el while
+				do {
+					$d = date("Y-m-d", strtotime("-$index day", time()));
+					$h = date("Y-m-d", strtotime("-$index day", time()));
+					$cierreDay = $this->cierre_model->getall($d, $h);
 					
-					//VERIFICAMOS REGISTROS DUPLICADOS CON FECHA Y CODIGO DIVISA EN LA TABLA CIERRE
-					if (!$this->cierre_model->get_check($fecha, $key["codigo"])) {
+					if($cierreDay){;
+						array_push($cierres, $cierreDay);
+					}
 
-						//verificamos el promedio para la cotizacion del dia
-						$cotizacion = 0;
-						foreach ($array as $arr){
-							if ( $arr['compras'] == 0 && $arr['ventas'] == 0 ){
-							continue;
-							} 
-							if($arr['codigo'] == $key["codigo"]){
-								$suma_gastos_compra = $suma_gastos_compra + $arr['gastos_compra'];
-								if($arr['gastos_compra']){
-									$cotizacion = number_format(round($arr['gastos_compra'] / $arr['compras'] , 4), 4);
-								} 
-							}
-						}
+					$index++;
+					
+				} while ($row[0] != $d && count($cierres) <= 5);
+			}
+		
+			$ope_cotizacion = operaciones_diarias($divisas, $operaciones, $ent_sal);
+			
+			if(!$cierres){
+				$cierres = 0;
+			}
 
-						//si la divisa es soles igualamos a 1 la cotizacion
-						if($key['codigo'] === 'PEN'){
-							$cotizacion = 1;
-						}
+			$suma_gastos_compra = 0; 
+			$porcentaje_compra_anterior = 0;
+			$porcentaje_compra = 0;
+			$peso_anterior = 0; //peso del valor porcentual
+			$peso = 0; //peso del valor porcentual
+			$index = 0;
+			$cotizacion = 0;
+			$cot = 0;
 
-						//si la cotizacion es 0 o no se ha registrado compras de la divisa 
-						//se iguala la cotizacion al tipo de cambio para calcular el valor en soles
-						if($cotizacion == 0){
-							$cotizacion = $key['cotizacion'];
-						}
-
-						$data = array(
-							'cod_divisa_cierre' => $key["codigo"],
-							'nom_divisa_cierre' => $key["nombre"],
-							'cot_cierre' => $cotizacion,
-							'can_cierre' => $key["caja"],
-							'fec_cierre' => $fecha,
-						);
-
+			foreach ($array as $arra){
+				foreach ($divisas as $key) {
+					
+					if ($arra['caja'] == 0) {
+						continue;
+					} 
+					
+					
+					if (!$this->cierre_model->get_check(date("Y-m-d"), $arra['codigo'])) {
 						
+						foreach ($cierres as $cie){
+						
+						
+							if($cie[$index]->cod_divisa_cierre === $arra['codigo']){
+								
+								//formula calcular cotizacion
+								//cantidad_cierre_anterior * 100 / cantidad _total;
+								
+								$porcentaje_compra_anterior = ($cie[$index]->can_cierre * 100) / $arra['caja'];
+								$peso_anterior = $porcentaje_compra_anterior * $cie[$index]->cot_cierre;
+								
+								foreach ($ope_cotizacion as $arr){
+								
+							
+									if ( $arr['compras'] == 0){
+									continue;
+									} 
+								
+									if($arr['codigo'] == $arra["codigo"]){
+									
+										$cotizacion = str_pad(round($arr['gastos_compra'] / $arr['compras'] , 4), 4);
+										$porcentaje_compra = ($arr['compras'] * 100) / $arra['caja'];
+										$peso = $cotizacion * $porcentaje_compra;
+										
+										$cot =  ($peso_anterior + $peso) / 100;
+										
+										//verificamos si ya se hizo el cierre del dia
+										if($cie[$index]->fec_cierre == date('Y-m-d')){
+											$cot = $cie[$index]->cot_cierre;
+										}
+								
+									}
+								}
+							
+							}
+						
+							//si no hay compras en el dia y la cotizacion es 0 se iguala al cierre anterior
+							if(!$cot){
+								$cot = $cie[$index]->cot_cierre;	
+							}
+
+							//si la divisa es soles igualamos a 1 la cotizacion
+							if($arra['codigo'] == 'PEN'){
+								$cot = 1;
+							} 
+
+							$index++;
+						}
+
+						//si no hay cierre anterior(primer dia)
+							
+						if($cierres === 0){ 
+								
+							$cot = $key->com_divisa;
+							foreach ($ope_cotizacion as $arr){
+								
+								if ( $arr['compras'] == 0){
+									continue;
+								} 
+								
+							
+								if($arr['codigo'] == $arra["codigo"] && $arra["cotizacion"] > 0){
+									$cot = str_pad(round($arr['gastos_compra'] / $arr['compras'] , 4), 4);
+								}
+								
+							}
+							
+						}
+						
+						if($arra['codigo'] == 'PEN'){
+							$cot = 1;
+						} 
+					
+						$data = array(
+							'cod_divisa_cierre' => $arra['codigo'],
+							'nom_divisa_cierre' => $arra['nombre'],
+							'cot_cierre' => $cot,
+							'can_cierre' => $arra["caja"],
+							'fec_cierre' => date("Y-m-d"),
+						);
+								
 						$datos = array(
 							'can_ganancia' => $this->ganancia(), 
-							'fec_ganancia' => $fecha,
+							'fec_ganancia' => date("Y-m-d"),
 						);
- 
 
-	
 						if (!$this->check_ganancia()) {
 
 							$this->ganancia_model->save($datos);
@@ -104,16 +183,26 @@ class Cierre extends CI_Controller {
 						}
 						
 						$this->cierre_model->save($data);
-					
-					}else{
 
-						// echo "<script>cierre_ejecutado();</script>";
-	
+
 					}
-	
-				}
+
+				}			 
+				
 				
 			}
+
+			
+					
+			// 		}else{
+
+			// 			// echo "<script>cierre_ejecutado();</script>";
+	
+			// 		}
+	
+			// 	}
+				
+			// }
 		redirect(base_url('admin/Admin_dashboard?err'));
 			
 		}else{
